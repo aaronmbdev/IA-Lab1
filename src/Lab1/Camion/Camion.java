a54ca2bc9162bee97baa69d2fe87c7ec6a617aa1
Aaron Morales <aaron@midokura.com>
2021-10-11 16:52:57 +0200

    Camion con metodos definidos, falta implementar

--- src/Lab1/Camion/Camion.java
+++ src/Lab1/Camion/Camion.java
@@ -16,4 +16,35 @@ public class Camion {
         this.estado = EstadoCisterna.VACIO;
         this.balance = 0;
     }
+
+    /**
+     * Método que ejecuta el desplazamiento del camión de su punto actual a destino
+     * @param destX Coordenada X del destino
+     * @param destY Coordenada Y del destino
+     */
+    public void desplazar(final int destX, final int destY) {
+
+    }
+
+    /**
+     * Método que retorna el camión a centro de distribución
+     */
+    public void volverABase() {
+
+    }
+
+    /**
+     * Método que llena el tanque del camión en centro de distribución
+     */
+    public void llenarTanque() {
+
+    }
+
+    /**
+     * Método que descarga la gasolina en gasolinera.
+     * @param depositos Cantidad de depósitos a llenar, debe ser entre 1 y 2.
+     */
+    public void llenarGasolinera(final int depositos) {
+        
+    }
 }
