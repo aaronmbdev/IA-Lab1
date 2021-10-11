602e601f26124056e2b9d0537d1628d902df8d5c
p-kami-sama <paul.gonzalez@estudiantat.upc.edu>
2021-10-11 16:56:12 +0200

    archivo de funcion heuristica

--- src/Lab1/GasolinaHeuristicFunction.java
+++ src/Lab1/GasolinaHeuristicFunction.java
@@ -1,11 +1,57 @@
 package Lab1;
 
-public class GasolinaHeuristicFunction {
+import aima.search.framework.HeuristicFunction;
+
+
+public class GasolinaHeuristicFunction implements HeuristicFunction{
 
     //implementa la función heurística
-    public double getHeuristicValue(Object n){
+    public double getHeuristicValue(Object state){
+        /*
+        * 1- Valores
+        * resultado = state.GananciasActuales
+        *
+        * 2- for aplicando a cada posible trayecto la transformacion X y luego calcular c
+        * ganancias_nuevas
+        *   3- if (ganancias_nuevas > resultado ){
+        *           resultado = ganancias_nuevas;
+        *           Cosa para guardar estado ???
+        *      }
+        *   //////////////////////////////
+        * 1- Suma de ganancias peticiones que cumple el estado
+        * 2- Resta de distancia que recorre los camiones con el estado actual
+        *
+        *   //////////////////////////////
+        *
+        * GasolinaBoard board = (GasolinaBoard)state;
+        *
+        * */
+        return 0.0;
 
-        return ((GasolinaBoard) n).heuristic();
     }
 
 }
+/*
+
+// heuristica problema del viajante
+
+public boolean equals(Object obj) {
+      boolean retValue;
+
+      retValue = super.equals(obj);
+      return retValue;
+  }
+
+  public double getHeuristicValue(Object state) {
+   ProbTSPBoard board=(ProbTSPBoard)state;
+   int sum=0,nc;
+   nc=board.getNCities();   //numero de ciudades
+
+   for(int i=0;i<nc;i++) sum=sum+board.distCities(i);
+
+   return (sum);
+  }
+
+
+
+* */
