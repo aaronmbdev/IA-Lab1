2a644eadba4eb9b24511740561d086130df03831
p-kami-sama <paul.gonzalez@estudiantat.upc.edu>
2021-10-23 16:05:13 +0200

    Primer sucedanio de main, Falta una buena generación de Sucesores

--- src/Lab1/GasolinerasDemo.java
+++ src/Lab1/GasolinerasDemo.java
@@ -6,8 +6,14 @@ import Lab1.Camion.Camion;
 import Lab1.Camion.CamionFactory;
 import Lab1.Peticiones.Peticion;
 import Lab1.Peticiones.PeticionFactory;
+import aima.search.framework.Problem;
+import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * This class is intented to be the starting point
@@ -21,8 +27,49 @@ public class GasolinerasDemo {
         List<Peticion> peticiones = PeticionFactory.fromGasolineras(gas);
         Estado inicial = new Estado(camiones,peticiones);
         System.out.println("Soy un genio");
+
+        ElectricaHillClimbingSearch(inicial);
     }
 
+
+
+
+
+    private static void ElectricaHillClimbingSearch( Estado board) {
+        System.out.println("\nHillClimbing  -->");
+        try {
+
+            Problem problem=new Problem(board,new GeneradorSucesores (), new GasolineraGoalTest(), new GasolinaHeuristicFunction());
+            Search search= new HillClimbingSearch();
+            SearchAgent agent = new SearchAgent (problem,search);
+
+
+            System.out.println();
+            printActions(agent.getActions());
+            printInstrumentation(agent.getInstrumentation());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private static void printInstrumentation(Properties properties) {
+        Iterator keys = properties.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String property = properties.getProperty(key);
+            System.out.println(key + " : " + property);
+        }
+
+    }
+
+    private static void printActions(List actions) {
+        for (int i = 0; i < actions.size(); i++) {
+            String action = (String) actions.get(i);
+            System.out.println(action);
+        }
+    }
 
 
 
